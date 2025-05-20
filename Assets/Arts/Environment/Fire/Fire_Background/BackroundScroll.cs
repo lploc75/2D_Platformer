1e164b44fba232aa33cbe3fce666464b8255d447
lploc75 <lpl070524@gmail.com>
2025-05-20 13:49:02 +0700

    first commit

---
+++ Assets/Arts/Environment/Fire/Fire_Background/BackroundScroll.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class ParallaxLayer : MonoBehaviour
+{
+    public float parallaxFactor = 0.5f; // Xa nhất: 0.1, gần nhất: 1
+    private Transform cam;
+    private Vector3 prevCamPos;
+
+    void Start()
+    {
+        cam = Camera.main.transform;
+        prevCamPos = cam.position;
+    }
+
+    void LateUpdate()
+    {
+        Vector3 delta = cam.position - prevCamPos;
+        transform.position += new Vector3(delta.x * parallaxFactor, delta.y * parallaxFactor, 0);
+        prevCamPos = cam.position;
+    }
+}
---
+++ Assets/Scripts/Shared/Player/Player.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    void Update()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal"); // A/D hoặc ←/→
+        float moveY = Input.GetAxisRaw("Vertical");   // W/S hoặc ↑/↓
+
+        Vector3 move = new Vector3(moveX, moveY, 0f);
+        transform.position += move * moveSpeed * Time.deltaTime;
+    }
+}
