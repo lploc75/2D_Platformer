c83dac21562b6c78bd088b97996c0d81e888be03
lploc75 <lpl070524@gmail.com>
2025-06-07 18:07:26 +0700

    Thêm máu và mana của nhân vật hoạt động trên UI

---
+++ Assets/Scripts/Shared/Player/HealthBar.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider healthSlider; // Gán trong Inspector
+    /*
+     Slider của mana min là 0 và max là 100, thay đổi bằng cách trừ máu tối đa - damgage nhận vào
+     */
+    public void SetMaxHealth(int maxHealth)
+    {
+        healthSlider.maxValue = maxHealth;
+        healthSlider.value = maxHealth;
+    }
+
+    public void SetHealth(int currentHealth)
+    {
+        if (healthSlider != null)
+        {
+            Debug.Log("Slider value set to " + currentHealth);
+            healthSlider.value = currentHealth;
+        }
+    }
+
+}
