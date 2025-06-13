b1c51663d88b352f803b6f4e21e46835e4e66e3e
KhanhPoi <khanhlcce181175@fpt.edu.vn>
2025-06-13 22:22:21 +0700

    Thêm hệ thống quản lý tiền tệ trong game

---
+++ Assets/Scripts/Data_concurrency/CurrencyManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CurrencyManager : MonoBehaviour
+{
+    public static CurrencyManager Instance { get; private set; }
+
+    // Quản lý số dư từng loại tiền
+    private Dictionary<CurrencyType, int> currencyAmounts = new Dictionary<CurrencyType, int>();
+
+    // UI cho từng loại tiền (kéo vào Inspector)
+    public Text coinText;
+    public Text gemText;
+    // Nếu thêm loại mới, khai báo UI ở đây
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+
+            // Khởi tạo số dư mặc định
+            foreach (CurrencyType type in System.Enum.GetValues(typeof(CurrencyType)))
+                currencyAmounts[type] = 0;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void AddCurrency(CurrencyType type, int amount)
+    {
+        currencyAmounts[type] += amount;
+        UpdateCurrencyUI(type);
+    }
+
+    public int GetCurrency(CurrencyType type)
+    {
+        return currencyAmounts[type];
+    }
+
+    private void UpdateCurrencyUI(CurrencyType type)
+    {
+        switch (type)
+        {
+            case CurrencyType.Coin:
+                if (coinText != null) coinText.text = currencyAmounts[type].ToString();
+                break;
+            case CurrencyType.Gem:
+                if (gemText != null) gemText.text = currencyAmounts[type].ToString();
+                break;
+                // Thêm UI cho loại mới tại đây nếu cần
+        }
+    }
+}
---
+++ Assets/Scripts/Data_concurrency/CurrencyType.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public enum CurrencyType
+{
+    Coin,
+    Gem,
+    // Thêm loại tiền mới ở đây nếu cần
+}
+
