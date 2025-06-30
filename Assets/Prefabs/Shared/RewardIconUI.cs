65eafe69e77d78d7017b0443cd14ee946f6640b6
Huy <Huyth.work@gmail.com>
2025-06-30 23:31:27 +0700

    Add reward UI

---
+++ Assets/Prefabs/Shared/RewardIconUI.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro; // Thêm dòng này nếu chưa có
+
+public class RewardIconUI : MonoBehaviour
+{
+    public Image qualityFrameImage; // Kéo QualityFrame vào đây
+    public Image iconImage;         // Kéo Icon vào đây
+    public TextMeshProUGUI amountText; // Thay vì Text
+
+    // Các Sprite viền (cài trong Inspector)
+    public Sprite commonFrame;
+    public Sprite rareFrame;
+    public Sprite epicFrame;
+    public Sprite legendaryFrame;
+
+    public void Setup(ItemData item, int amount = 1)
+    {
+        iconImage.sprite = item.icon;
+        amountText.text = "x" + amount;
+
+        switch (item.quality)
+        {
+            case ItemQuality.Common: qualityFrameImage.sprite = commonFrame; break;
+            case ItemQuality.Rare: qualityFrameImage.sprite = rareFrame; break;
+            case ItemQuality.Epic: qualityFrameImage.sprite = epicFrame; break;
+            case ItemQuality.Legendary: qualityFrameImage.sprite = legendaryFrame; break;
+            default: qualityFrameImage.sprite = commonFrame; break;
+        }
+    }
+}
