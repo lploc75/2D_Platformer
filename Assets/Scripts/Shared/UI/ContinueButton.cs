623525034ab02d371d2761572c2df19176b066bd
Huy <Huyth.work@gmail.com>
2025-07-18 12:35:55 +0700

    Thêm nút continue

---
+++ Assets/Scripts/Shared/UI/ContinueButton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ContinueButton : MonoBehaviour
+{
+    [Tooltip("Tên hoặc index scene muốn tải khi Continue")]
+    public string sceneName = "Village";
+
+    [Tooltip("Chờ bao lâu trước khi load (để âm thanh click phát xong)")]
+    public float delay = 0.5f;
+
+    // Gọi hàm này cho nút CONTINUE
+    public void OnClickContinue()
+    {
+        if (GameSaveManager.Instance != null)
+        {
+            GameSaveManager.Instance.LoadGame();
+            if (delay > 0f)
+                StartCoroutine(ContinueAfterDelay());
+            else
+                SceneManager.LoadScene(sceneName);
+        }
+        else
+        {
+            Debug.LogError("Không tìm thấy GameSaveManager.Instance!");
+        }
+    }
+
+    IEnumerator ContinueAfterDelay()
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(sceneName);
+    }
+}
